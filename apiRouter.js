2eebba3ab1a9b119dde2fc9715d2d334c8a2f647
matgo <matthias.goupil@smile.fr>
2023-03-30 19:11:46 +0200

    Projet fini, supression de l'upload d'images

--- apiRouter.js
+++ apiRouter.js
@@ -3,10 +3,8 @@ const router = new express.Router()
 
 const userActions = require("./routes/userActions");
 const productActions = require("./routes/productActions");
-const imageActions = require("./routes/imageActions");
 
 const {middlewareAuthentication} = require("./utils/jwt.util")
-const {middlewareUploadImages} = require("./utils/upload.util");
 
 // Route which allows user to login and get a JWT token 
 router.post("/user/login",userActions.login)
@@ -20,10 +18,7 @@ router.get("/products",productActions.getAll) // TO GET a product
 router.get("/products/:id",productActions.get) // To GET the product list
 router.delete("/products/:id",middlewareAuthentication,productActions.delete) // To DELETE a product
 router.patch("/products/:id",middlewareAuthentication,productActions.update) // To UPDATE a product
-router.post("/products",[middlewareAuthentication, middlewareUploadImages],productActions.create) // To CREATE a product
-
-// GET an image
-router.get('/images/:name',imageActions.get)
+router.post("/products",middlewareAuthentication,productActions.create) // To CREATE a product
 
 module.exports = {
     router
